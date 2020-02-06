8e9a4a45caa12dd7a986a5790bfada02fdc6e5f0
issacholguin <issacjamesholguin@gmail.com>
2020-02-06 05:07:11 -0800

    FlatList Section Complete

--- src/screens/ListScreen.js
+++ src/screens/ListScreen.js
@@ -1,14 +1,58 @@
 // Part 1: import libaries
 import React from 'react';
-import { View, Text, StyleSheet } from 'react-native';
+import { View, Text, StyleSheet, FlatList } from 'react-native';
 
 // Part 2: create component, function that returns JSX
 const ListScreen = () => {
-    return <Text>List Screen</Text>;
+    const friends = [
+        // Adding Keys: could add to each specific object:
+        // Req: Must be a string, and must be unique
+        // { name: 'Friend #1', key: '1'},
+        // { name: 'Friend #2', key: '2'},
+        // In my opinion, not the best way...
+        { name: 'Friend #1'},
+        { name: 'Friend #2'},
+        { name: 'Friend #3'},
+        { name: 'Friend #4'},
+        { name: 'Friend #5'},
+        { name: 'Friend #6'},
+        { name: 'Friend #7'},
+        { name: 'Friend #8'},
+        { name: 'Friend #9'},
+    ]
+    return (
+        <FlatList
+          // Prop: 'horizontal' --> telling our list we want to scroll horizontally instead of vertically
+          // we could simply put 'horizontal', or 'horizontal={true}'
+          horizontal
+
+          // Prop: 'showsHorizontalScrollIndicator' --> shows the horizontal scroll bar or not
+          // Ex: showsHorizontalScrollIndicator={true}
+          // Ex: showsHorizontalScrollIndicator={false}
+          showsHorizontalScrollIndicator={false}
+
+          // Another way to add a key: keyExtractor on the FlatList element itself
+          // This way is defining our keyProperty at runtime vs ahead of time
+          // Favoring keyExtractor
+          keyExtractor={(friend) => friend.name}
+          data={friends}
+          renderItem={({item}) => {
+            // if (element) were our parameter, we would get this data back:
+            // element === {item: {name: 'Friend #1'}, index: 0}
+            // but we could use ES2015 desctructuring, and just put ({item}), where we extract just the item we want
+            // ({item}) === {name: 'Friend #1'}
+            return <Text style={styles.textStyle}>{item.name}</Text>;
+            }}
+        />
+    );
 }
 
 // Part 3: create styles
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+    textStyle: {
+        marginVertical: 50
+    }
+});
 
 // Part 4: export component
 export default ListScreen;
