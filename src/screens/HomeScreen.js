5e4f8369a54ef86b48fe741819e740fa98918b81
issacholguin <issacjamesholguin@gmail.com>
2020-02-07 05:20:07 -0800

    using props/navigation!

--- src/screens/HomeScreen.js
+++ src/screens/HomeScreen.js
@@ -1,21 +1,29 @@
 import React from 'react';
 import { Text, StyleSheet, View, Button, TouchableOpacity } from 'react-native';
 
-const HomeScreen = () => {
+const HomeScreen = ({ navigation }) => {
+  console.log(navigation);
+
   return (
     <View>
       <Text style={styles.text}>HomeScreen</Text>
       {/* For buttons we don't pass in text, but we put it in the title prop */}
       <Button
         title="Go to Components Demo"
-        onPress={() => console.log('Button Pressed')}
+        onPress={() => navigation.navigate('Components')}
       />
-      <TouchableOpacity onPress={() => console.log('List Pressed')}>
+
+      <Button
+        title="Go to List Demo"
+        onPress={() => navigation.navigate('List')}
+      />
+      
+      {/* <TouchableOpacity onPress={() => props.navigation.navigate('List')}>
         <Text>Go to List Demo</Text>
         <Text>Go to List Demo</Text>
         <Text>Go to List Demo</Text>
         <Text>Go to List Demo</Text>
-      </TouchableOpacity>
+      </TouchableOpacity> */}
     </View>
   );
 };
